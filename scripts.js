9ecb5e775c04c2d30abf9c6668989b9cf15220c9
Chris Bates <Chrisbates79@icloud.com>
2016-04-01 15:11:29 -0400

    added more logic

--- scripts.js
+++ scripts.js
@@ -1,15 +1,23 @@
 $(document).ready(function() {
   console.log("working");
 
+  var count = 1;
   var $gamecells = $(".square");
+  var moves = ["", "", "", "", "", "", "", "", ""];
+
   $($gamecells).one('click', function() {
-    console.log(this.id);
+    count ++;
+    moves[this.id] = this.id;
+
+    if(count % 2 === 0){
+      moves[this.id] ='X';
+    }
+    else {
+      moves[this.id] ='O';
+    }
+    console.log(moves,count);
 
   })
 
 
 })
-
-
-
-
