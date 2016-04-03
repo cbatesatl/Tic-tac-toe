1d71d83bf3087332819568e54aee19f981949661
Chris Bates <Chrisbates79@icloud.com>
2016-04-03 17:18:54 -0400

    wrote logic to check for tie

--- scripts.js
+++ scripts.js
@@ -16,16 +16,18 @@ $(document).ready(function() {
       turn = 'O';
     }
     console.log(moves, count, turn);
-
+    //long conditonal statements, try to condense later
     if (moves[0] === "X" && moves[1] === "X" && moves[2] === "X" || moves[3] === "X" && moves[4] === "X" && moves[5] === "X" || moves[6] === "X" && moves[7] === "X" && moves[8] === "X" || moves[0] === "X" && moves[3] === "X" && moves[6] === "X" || moves[1] === "X" && moves[4] === "X" && moves[7] === "X" || moves[2] === "X" && moves[5] === "X" && moves[8] === "X" || moves[0] === "X" && moves[4] === "X" && moves[8] === "X" || moves[2] === "X" && moves[4] === "X" && moves[6] === "X") {
-      alert("X wins"),location.reload();
+      alert("X WINS!!!!!!"), location.reload();
     } else if (moves[0] === "O" && moves[1] === "O" && moves[2] === "O" || moves[3] === "O" && moves[4] === "O" && moves[5] === "O" || moves[6] === "O" && moves[7] === "O" && moves[8] === "O" || moves[0] === "O" && moves[3] === "O" && moves[6] === "O" || moves[1] === "O" && moves[4] === "O" && moves[7] === "O" || moves[2] === "O" && moves[5] === "O" && moves[8] === "O" || moves[0] === "O" && moves[4] === "O" && moves[8] === "O" || moves[2] === "O" && moves[4] === "O" && moves[6] === "O") {
-      alert("O wins"),location.reload();
-    }
+      alert("O WINS!!!!!!"), location.reload();
+      }
+      else if (count === 9){
+       alert("TIE!!!"),location.reload();
+      }
+
   })
-
-
-  $(".reset").click(function() {
+   $(".reset").click(function() {
     location.reload();
   })
 })
