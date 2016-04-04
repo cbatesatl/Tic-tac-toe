0e579b7899937fffe4a3900e6e33367a4ba60a8d
Chris Bates <Chrisbates79@icloud.com>
2016-04-04 13:17:46 -0400

    made check winner a function

--- scripts.js
+++ scripts.js
@@ -16,21 +16,36 @@ $(document).ready(function() {
       turn = 'O';
     }
     console.log(moves, count, turn);
-    //long conditonal statements, try to condense later
+    $($gamecells).on('click',checkWinner());
 
-    if (moves[0] === "X" && moves[1] === "X" && moves[2] === "X" || moves[3] === "X" && moves[4] === "X" && moves[5] === "X" || moves[6] === "X" && moves[7] === "X" && moves[8] === "X" || moves[0] === "X" && moves[3] === "X" && moves[6] === "X" || moves[1] === "X" && moves[4] === "X" && moves[7] === "X" || moves[2] === "X" && moves[5] === "X" && moves[8] === "X" || moves[0] === "X" && moves[4] === "X" && moves[8] === "X" || moves[2] === "X" && moves[4] === "X" && moves[6] === "X") {
-       $("h2").html("X WINS!!!"),turn = null;
-    } else if (moves[0] === "O" && moves[1] === "O" && moves[2] === "O" || moves[3] === "O" && moves[4] === "O" && moves[5] === "O" || moves[6] === "O" && moves[7] === "O" && moves[8] === "O" || moves[0] === "O" && moves[3] === "O" && moves[6] === "O" || moves[1] === "O" && moves[4] === "O" && moves[7] === "O" || moves[2] === "O" && moves[5] === "O" && moves[8] === "O" || moves[0] === "O" && moves[4] === "O" && moves[8] === "O" || moves[2] === "O" && moves[4] === "O" && moves[6] === "O") {
-      $("h2").html("O WINS!!!"),turn = null;
+    function checkWinner(){
+    if (moves[0] === "X" && moves[1] === "X" && moves[2] === "X" ||
+      moves[3] === "X" && moves[4] === "X" && moves[5] === "X" ||
+      moves[6] === "X" && moves[7] === "X" && moves[8] === "X" ||
+      moves[0] === "X" && moves[3] === "X" && moves[6] === "X" ||
+      moves[1] === "X" && moves[4] === "X" && moves[7] === "X" ||
+      moves[2] === "X" && moves[5] === "X" && moves[8] === "X" ||
+      moves[0] === "X" && moves[4] === "X" && moves[8] === "X" ||
+      moves[2] === "X" && moves[4] === "X" && moves[6] === "X") {
+      $("h2").html("X WINS!!!"), turn = null;
+    } else if (moves[0] === "O" && moves[1] === "O" && moves[2] === "O" ||
+      moves[3] === "O" && moves[4] === "O" && moves[5] === "O" ||
+      moves[6] === "O" && moves[7] === "O" && moves[8] === "O" ||
+      moves[0] === "O" && moves[3] === "O" && moves[6] === "O" ||
+      moves[1] === "O" && moves[4] === "O" && moves[7] === "O" ||
+      moves[2] === "O" && moves[5] === "O" && moves[8] === "O" ||
+      moves[0] === "O" && moves[4] === "O" && moves[8] === "O" ||
+      moves[2] === "O" && moves[4] === "O" && moves[6] === "O") {
+      $("h2").html("O WINS!!!"), turn = null;
     } else if (count === 9) {
-      $("h2").html("TIE!!!"),turn = null;
+      $("h2").html("TIE!!!"), turn = null;
     }
-
+}
   })
 
 
   $(".reset").click(function() {
-   location.reload();
+    location.reload();
 
   })
 })
