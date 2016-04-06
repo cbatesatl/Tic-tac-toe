f052a73ea3d0a7e9e89e2dda6788726c12aaa51f
Chris Bates <Chrisbates79@icloud.com>
2016-04-06 10:03:16 -0400

    fixed reset bug

--- scripts.js
+++ scripts.js
@@ -10,6 +10,8 @@ $(document).ready(function() {
   //var nameX = prompt("Player X enter your name");
   //var nameO = prompt("Player O enter your name");
 
+  //Hides new game button until a winner is found
+  $("#newgame").hide();
   //resets gameboard
   $(".reset").click(function() {
       $($gamecells).html("");
@@ -17,6 +19,7 @@ $(document).ready(function() {
       turn = "X";
       moves = ["", "", "", "", "", "", "", "", ""];
       $("h2").html("X Goes First!");
+      $("#newgame").hide();
       game();
       console.log(moves, count, turn);
 
@@ -67,7 +70,7 @@ $(document).ready(function() {
           moves[2] === "X" && moves[5] === "X" && moves[8] === "X" ||
           moves[0] === "X" && moves[4] === "X" && moves[8] === "X" ||
           moves[2] === "X" && moves[4] === "X" && moves[6] === "X") {
-          $("h2").html("X WINS!!!"), $($gamecells).off(), xWins++, $("#xscore").html('"X" Score ' + xWins);
+          $("h2").html("X WINS!!!"), $($gamecells).off(), xWins++, $("#xscore").html('"X" Score ' + xWins), $("#newgame").show();;
         } else if (moves[0] === "O" && moves[1] === "O" && moves[2] === "O" ||
           moves[3] === "O" && moves[4] === "O" && moves[5] === "O" ||
           moves[6] === "O" && moves[7] === "O" && moves[8] === "O" ||
@@ -76,20 +79,20 @@ $(document).ready(function() {
           moves[2] === "O" && moves[5] === "O" && moves[8] === "O" ||
           moves[0] === "O" && moves[4] === "O" && moves[8] === "O" ||
           moves[2] === "O" && moves[4] === "O" && moves[6] === "O") {
-          $("h2").html("O WINS!!!"), $($gamecells).off(), oWins++, $("#oscore").html('"O" Score ' + oWins);
+          $("h2").html("O WINS!!!"), $($gamecells).off(), oWins++, $("#oscore").html('"O" Score ' + oWins), $("#newgame").show();;
         } else if (count === 9) {
-          $("h2").html("TIE!!!"), $($gamecells).off(), console.log("Tie");
+          $("h2").html("TIE!!!"), $($gamecells).off(), console.log("Tie"), $("#newgame").show();
         }
+        /*
+                function checkPlayer() {
+                  if (turn = 'X') {
+                    $("h2").html("X's Turn");
+                  } else if (turn = "X") {
+                    $("h2").html("O's Turn");
+                  }
 
-        function checkPlayer() {
-          if (turn = 'X') {
-            $("h2").html("X's Turn");
-          } else if (turn = "X") {
-            $("h2").html("O's Turn");
-          }
-
-        }
-
+                }
+        */
 
       }
 
